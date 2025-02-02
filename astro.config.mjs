ed53b00f022c6f73c3c42c9e5e386db63edfd686
jhonyspicy <jhonyspicy@gmail.com>
2025-02-03 02:35:15 +0900

    ローカル環境と本番環境でパスが違うことに対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jhonyspicy.github.io',
-  base: '/gardener-sample',
   integrations: [
     tailwind(),
   ],
   server: {
     port: 8089
-  }
+  },
+  base: process.env.NODE_ENV === 'production' 
+    ? '/gardener-sample'  // 本番環境（GitHub Pages）用のパス
+    : ''                  // ローカル環境用のパス
 });
